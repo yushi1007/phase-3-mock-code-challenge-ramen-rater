f16e3bf752d616edabb9cd6fbe605645d79aafaf
Yu Shi <yushiys95@gmail.com>
2021-03-23 12:26:59 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,67 @@
 // write your code here
-// hello
+
+const body = document.querySelector('body')
+const ramenMenu = body.querySelector('div#ramen-menu')
+const url = "http://localhost:3000/ramens"
+const ramenInfo = body.querySelector('div#ramen-detail')
+
+
+
+const renderRamenImage = ramenObject => {
+          
+         ramenMenu.innerHTML +=
+         `<img class="detail-image" src="${ramenObject.image}" alt="${ramenObject.name}" data-id=${ramenObject.id}/>`
+}
+
+const addRamenToMenu = ramenObject => {
+    ramenMenu.innerHTML = ""
+    ramenObject.forEach(ramen => {
+        renderRamenImage(ramen)
+    })
+}
+
+const renderRamenInfo = ramenObject => {
+
+        ramenInfo.innerHTML = `      
+        <img class="detail-image" src="${ramenObject.image}" alt="${ramenObject.name}" />
+        <h2 class="name">${ramenObject.name}</h2>
+        <h3 class="restaurant">${ramenObject.restaurant}</h3>`
+
+    const ramenRating = document.querySelector('#ramen-rating')
+        ramenRating.rating.value = ramenObject.rating
+        ramenRating.comment.value =ramenObject.comment
+}
+
+
+const fetchAllRamen = event => {
+        // const ramenId = event.dataset.id
+
+        fetch(url)
+            .then(response => response.json())
+            .then(ramenObjects => { 
+                addRamenToMenu(ramenObjects)
+                })
+
+}
+
+ 
+
+const fetchOneRamen = event => {
+    const ramenId = event.dataset.id
+
+    fetch(`${url}/${ramenId}`)
+        .then(response => response.json())
+        .then(ramenInfo => { 
+            renderRamenInfo(ramenInfo)
+            })
+
+}
+
+ramenMenu.addEventListener('click', event => {
+    if (event.target.matches('img')) {
+
+        fetchOneRamen(event.target)
+    }
+})
+
+fetchAllRamen()
